084da9e6cd782611e35c2e162df98153e8750c35
Katherine Kelly <ktkelly11@gmail.com>
2024-06-19 12:24:25 -0400

    Figured out how to keep images from repeating

--- src/components/Paintings.jsx
+++ src/components/Paintings.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import "./Paintings.css";
 
 function Paintings() {
   const api = import.meta.env.VITE_X_API_KEY;
@@ -8,7 +9,7 @@ function Paintings() {
     async function fetchAPI() {
       try {
         const response = await fetch(
-          "https://api.europeana.eu/record/v2/search.json?query=DaVinci",
+          "https://api.europeana.eu/record/v2/search.json?query=Manet",
           {
             headers: {
               "X-Api-Key": api,
@@ -28,16 +29,22 @@ function Paintings() {
     <>
       {paintings.map((items) => (
         <div key={items.id}>
-          {paintings.length === 0
-            ? ""
-            : paintings.map((painting) => {
-                return <img src={painting.edmPreview[0]} />;
-              })}
-
+          <div key="paintings" className="paintings">
+            <img src={items.edmPreview[0]} alt="" />
+            {/* {paintings.length === 0
+              ? ""
+              : paintings.map((painting) => {
+                  return <img src={painting.edmPreview[0]} />;
+                })} */}
+          </div>
           {/* Supposed to display title in English */}
           {/* <p>{items.dcTitleLangAware.en}</p> */}
-          <p>{items.country}</p>
-          <p>{items.dataProvider}</p>
+          <p key="country" className="country">
+            {items.country}
+          </p>
+          <p key="museum" className="museum">
+            {items.dataProvider}
+          </p>
           {/* This should give an English description: */}
           {/* <p>{items.dcDescriptionLangAware.en}</p> */}
           {/* {items.length === 0 ? "" : items.map((items) => {
