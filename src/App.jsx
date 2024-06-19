3d4f9700cb840ff1031d7bd1485ff267d2e4eb78
Katherine Kelly <ktkelly11@gmail.com>
2024-06-19 09:57:05 -0400

    Added useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,22 @@ import Navbar from "./components/NavBar.jsx";
 function App() {
   const url = import.meta.env.VITE_API_URL;
   // console.log(url);
+  const api = import.meta.env.VITE_X_API_KEY;
+  const [paintings, setPaintings] = useState([]);
+
+  useEffect(() => {
+    async function fetchAPI() {
+      try {
+        const response = await fetch(url, {
+          headers: {
+            "X-Api-Key": api,
+          },
+        });
+      } catch (err) {
+        console.log(err);
+      }
+    }
+  }, []);
 
   return (
     <>
