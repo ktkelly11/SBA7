cb5787e604cf0f139d5ee7f601f27460a9e4355c
Katherine Kelly <ktkelly11@gmail.com>
2024-06-20 15:39:40 -0400

    Finished reducer function

--- src/utils/reducerFn.jsx
+++ src/utils/reducerFn.jsx
@@ -1,1 +1,14 @@
-import { useReducer } from "react";
+import { reducerFn } from "./utils/reducerFn";
+
+reducerFn = (state, action) => {
+  switch ((action, type)) {
+    case "Increment":
+      return state + 1;
+
+    case "Decrement":
+      return state - 1;
+
+    default:
+      throw Error("Unknown action " + action.type);
+  }
+};
